72a7e2f69a580584e2628902132727b9499ffc54
Rybeusz100 <janzych@protonmail.com>
2023-01-19 00:58:40 +0100

    add time_and_print macro

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
-mod aoc_2022;
+use std::time::Instant;
 
+mod aoc_2022;
 use aoc_2022::*;
 
+macro_rules! time_and_print {
+    ($message:expr, $func:expr) => {
+        let start = Instant::now();
+        let result = $func;
+        let end = Instant::now();
+        println!("{} in {:?}", $message, end.duration_since(start));
+        println!("{:#?}", result);
+        println!();
+    };
+}
+
 fn main() {
-    println!("{:#?}", aoc_2022_1::solution().unwrap());
-    println!("{:#?}", aoc_2022_2::solution().unwrap());
-    println!("{:#?}", aoc_2022_3::solution().unwrap());
-    println!("{:#?}", aoc_2022_4::solution().unwrap());
-    println!("{:#?}", aoc_2022_5::solution().unwrap());
-    println!("{:#?}", aoc_2022_6::solution().unwrap());
+    time_and_print!("AoC 2022 day 1", aoc_2022_1::solution().unwrap());
+    time_and_print!("AoC 2022 day 2", aoc_2022_2::solution().unwrap());
+    time_and_print!("AoC 2022 day 3", aoc_2022_3::solution().unwrap());
+    time_and_print!("AoC 2022 day 4", aoc_2022_4::solution().unwrap());
+    time_and_print!("AoC 2022 day 5", aoc_2022_5::solution().unwrap());
+    time_and_print!("AoC 2022 day 6", aoc_2022_6::solution().unwrap());
 }
